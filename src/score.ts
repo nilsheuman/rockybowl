2ae6c4425b2a5e8da17d27b49ff7cab24d289a51
Nils Heuman <nilsheuman@gmail.com>
2025-03-13 10:17:19 +1000

    call rolls frames in scoring

--- src/score.ts
+++ src/score.ts
@@ -1,22 +1,22 @@
 import { PINS, Frame } from "./types";
 import { sum } from "./utils";
 
-export function calculateScore(index: number, rolls: Frame[]) {
-    const first = rolls[index][0] || 0;
-    const second = rolls[index][1] || 0;
+export function calculateScore(index: number, frames: Frame[]) {
+    const first = frames[index][0] || 0;
+    const second = frames[index][1] || 0;
     let score = first + second;
     if (first == PINS) {
         // strike, add two next frames
-        if (rolls[index + 1]) {
-            score += sum(rolls[index + 1]);
+        if (frames[index + 1]) {
+            score += sum(frames[index + 1]);
         }
-        if (rolls[index + 2]) {
-            score += sum(rolls[index + 2]);
+        if (frames[index + 2]) {
+            score += sum(frames[index + 2]);
         }
     } else if (first + second == PINS) {
         // spare, add next frame
-        if (rolls[index + 1]) {
-            score += sum(rolls[index + 1]);
+        if (frames[index + 1]) {
+            score += sum(frames[index + 1]);
         }
     }
     return score;
